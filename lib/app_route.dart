ad57716f1497b8fef4f5ea616ecc0513611b2026
IhabMks <ihab.ozark@gmail.com>
2023-08-20 21:58:40 +0200

    relocated routes's map

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,11 +1,27 @@
+import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/pages/login_page.dart';
+import 'package:chatapp/pages/profile_page.dart';
+import 'package:chatapp/pages/search_page.dart';
+import 'package:chatapp/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
 class AppRouteManager {
   static const String loginRoute = "/login";
   static const String signUpRoute = "/signup";
   static const String homeRoute = "/home";
-  static const String sarchRoute = "/search";
+  static const String searchRoute = "/search";
   static const String profileRoute = "/profile";
+  static const String chatRoute = "/chat";
+
+  static Map<String, Widget Function(BuildContext)> generateRouteMap() {
+    return {
+      loginRoute: (context) => const LoginPage(),
+      homeRoute: (context) => const HomePage(),
+      signUpRoute: (context) => const SignUpPage(),
+      searchRoute: (context) => const SearchPage(),
+      profileRoute: (context) => const ProfilePage(),
+    };
+  }
 
   static void nextScreen(BuildContext context, Widget page) {
     Navigator.push(context, MaterialPageRoute(builder: (context) => page));
